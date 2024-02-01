d40ee406fba9e914bd778ba6f05ea1a6a6283ee8
Samuel Renaud <2392263@csfoy.ca>
2024-02-01 14:00:08 -0500

    Redo solution for WinForms app

---
+++ POOII_Module12_TraitementImages/POOII_Module12_TraitementImages/Form1.cs
@@ -0,0 +1,10 @@
+namespace POOII_Module12_TraitementImages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
